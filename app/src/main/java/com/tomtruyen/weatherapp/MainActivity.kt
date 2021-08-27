2c0ad4acfddb0154fb501e2aeb102338974064db
TomTruyen <tom.truyen@gmail.com>
2021-08-27 17:24:58 +0200

    Add basic icons

--- app/src/main/java/com/tomtruyen/weatherapp/MainActivity.kt
+++ app/src/main/java/com/tomtruyen/weatherapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.tomtruyen.weatherapp
 
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.Animatable2.AnimationCallback
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.tomtruyen.weatherapp.services.WeatherService
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +19,21 @@ class MainActivity : AppCompatActivity() {
         mWeatherService = WeatherService(this)
         mWeatherService.mLocation = "Bree"
         mWeatherService.request()
+
+        startAnimations()
+    }
+
+    private fun startAnimations(registerCallback: Boolean = true) {
+        val drawable = findViewById<ImageView>(R.id.image).drawable as AnimatedVectorDrawable
+
+        if(registerCallback) {
+            drawable.registerAnimationCallback(object : AnimationCallback() {
+                override fun onAnimationEnd(drawable: Drawable) {
+                    startAnimations(false)
+                }
+            })
+        }
+
+        drawable.start()
     }
 }
